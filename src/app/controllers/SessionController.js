d5a7e82ad7a790a49ee21ea73553fe645550ac9c
Uraquitan Filho <uraquitan@syx.com.br>
2019-07-04 05:49:37 -0300

    validation using yup

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,28 @@
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
+import * as Yup from 'yup';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    /**
+     * Using Yup to validate data
+     */
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(400)
+        .json({ error: 'Validation fails. Check email and password' });
+    }
+
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
     /**
